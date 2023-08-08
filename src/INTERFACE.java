ddd855274a8fe4708a4d7d42bdbc42a5d395b95a
Hector Perez <hector.mario.peraz@gmail.com>
2023-08-08 18:46:11 +0200

    add loop while for show the client responsive

--- src/INTERFACE.java
+++ src/INTERFACE.java
@@ -3,13 +3,37 @@ import java.util.Scanner;
 public class INTERFACE{
     public static void main(String[] args) {
         Scanner client = new Scanner(System.in);
-        System.out.println("Please select an option:");
-        System.out.println("Select 1 to inquire about your account balance");
-        System.out.println("Select 2 to deposit cash");
-        System.out.println("Select 3 to withdraw cash");
-        System.out.println("Select 4 to view your latest transactions");
-        System.out.print("Select an option and press enter: ");
-        int option = client.nextInt();
-        System.out.println("You have selected option: " + option);
+        boolean controlWhile = true ;
+        String OPTION1 = "inquire about your account balance";
+        String OPTION2 = "deposit cash";
+        String OPTION3 = "withdraw cash";
+        String OPTION4 = "view your latest transactions";
+
+        while (controlWhile){
+            System.out.println("Please select an option:");
+            System.out.println("Select 1 to "+OPTION1);
+            System.out.println("Select 1 to "+OPTION2);
+            System.out.println("Select 1 to "+OPTION3);
+            System.out.println("Select 1 to "+OPTION4);
+            System.out.print("Select an option or other key for exit and press enter: ");
+            int option = client.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("You have selected option: " + OPTION1);
+                    break;
+                case 2:
+                    System.out.println("You have selected option: " + OPTION2);
+                    break;
+                case 3:
+                    System.out.println("You have selected option: " + OPTION3);
+                    break;
+                case 4:
+                    System.out.println("You have selected option: " + OPTION4);
+                    break;
+                default:
+                    System.out.println("Good bye, thanks for use us services");
+                    controlWhile = false;
+            }
+        }
     }
 }
